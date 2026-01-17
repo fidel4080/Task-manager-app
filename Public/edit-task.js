1afaaf130d672237b66f913bbb9ececfac3b2111
Fidel Castro <fidelcastro4080@gmail.com>
2026-01-17 19:41:28 +0300

    removed axios

--- Public/edit-task.js
+++ Public/edit-task.js
@@ -4,62 +4,75 @@ const taskCompletedDOM = document.querySelector('.task-edit-completed')
 const editFormDOM = document.querySelector('.single-task-form')
 const editBtnDOM = document.querySelector('.task-edit-btn')
 const formAlertDOM = document.querySelector('.form-alert')
+
 const params = window.location.search
 const id = new URLSearchParams(params).get('id')
-let tempName
 
+let tempName;
+
+// show single task
 const showTask = async () => {
   try {
-    const {
-      data: { task },
-    } = await axios.get(`/api/v1/tasks/${id}`)
+    const res = await fetch(`/api/v1/tasks/${id}`)
+    const data = await res.json()
+    const task = data.task
+
     const { _id: taskID, completed, name } = task
 
     taskIDDOM.textContent = taskID
     taskNameDOM.value = name
     tempName = name
-    if (completed) {
-      taskCompletedDOM.checked = true
-    }
+    taskCompletedDOM.checked = completed
   } catch (error) {
-    console.log(error)
+    console.error(error)
   }
 }
 
 showTask()
 
+// update task
 editFormDOM.addEventListener('submit', async (e) => {
-  editBtnDOM.textContent = 'Loading...'
   e.preventDefault()
+  editBtnDOM.textContent = 'Loading...'
+
   try {
     const taskName = taskNameDOM.value
     const taskCompleted = taskCompletedDOM.checked
 
-    const {
-      data: { task },
-    } = await axios.patch(`/api/v1/tasks/${id}`, {
-      name: taskName,
-      completed: taskCompleted,
+    const res = await fetch(`/api/v1/tasks/${id}`, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        name: taskName,
+        completed: taskCompleted,
+      }),
     })
+
+    const data = await res.json()
+    const task = data.task
 
     const { _id: taskID, completed, name } = task
 
     taskIDDOM.textContent = taskID
     taskNameDOM.value = name
     tempName = name
-    if (completed) {
-      taskCompletedDOM.checked = true
-    }
+    taskCompletedDOM.checked = completed
+
     formAlertDOM.style.display = 'block'
-    formAlertDOM.textContent = `success, edited task`
+    formAlertDOM.textContent = 'success, edited task'
     formAlertDOM.classList.add('text-success')
+
   } catch (error) {
     console.error(error)
     taskNameDOM.value = tempName
     formAlertDOM.style.display = 'block'
-    formAlertDOM.innerHTML = `error, please try again`
+    formAlertDOM.textContent = 'error, please try again'
   }
+
   editBtnDOM.textContent = 'Edit'
+
   setTimeout(() => {
     formAlertDOM.style.display = 'none'
     formAlertDOM.classList.remove('text-success')
