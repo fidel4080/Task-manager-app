d76b4927b96ee0cf6052c27326c5af861112542d
Fidel Castro <fidelcastro4080@gmail.com>
2026-01-22 12:51:00 +0300

    updated files

--- controllers/tasks.controller.js
+++ controllers/tasks.controller.js
@@ -1,30 +1,29 @@
 const Task = require('../models/tasks.model.js')
 const asyncWrapper = require('../middlewares/async.js');
+const {createCustomError} = require('../errors/custom-error.js');
 
 const getAllTasks = asyncWrapper(async (req, res)=> {
         const tasks = await Task.find({});
         res.status(200).json({tasks});
 });
 
-const createTask = asyncWrapper( async (req, res)=> {
+const createTask = asyncWrapper( async (req, res, next)=> {
         const task = await Task.create(req.body);
         res.status(201).json({task});
 });
 
-const getTask = asyncWrapper(async (req, res)=> {
+const getTask = asyncWrapper(async (req, res, next)=> {
     const {id: taskID} = req.params;
     const task = await Task.findOne({_id:taskID});
 
     if(!task){
-        return res.status(404).json({
-            message:   `No Task with ID: ${taskID} was found`
-        });
+        return next(createCustomError(`No Task with ID: ${taskID} was found`, 404));
     }
 
     res.status(200).json({task});
 });
 
-const updateTask = asyncWrapper(async (req, res)=> {
+const updateTask = asyncWrapper(async (req, res, next)=> {
         const {id: taskID} = req.params;
         const task = await Task.findOneAndUpdate(
             {_id: taskID},
@@ -36,26 +35,22 @@ const updateTask = asyncWrapper(async (req, res)=> {
         );
 
         if(!task){
-            return res.status(404).json({
-                message: 'no task with that id was found'
-            });
+            return next(createCustomError(`No task with ID: ${taskID} was found`, 404));
         }
 
         res.status(200).json({task});
 });
 
-const deleteTask = asyncWrapper(async (req, res)=> {
+const deleteTask = asyncWrapper(async (req, res, next)=> {
         const {id: taskID} = req.params;
         const task = await Task.findOneAndDelete({_id: taskID});
 
         if(!task){
-            res.status(404).json({
-                message: `No task with id: ${taskID} was found`
-            });
+            return next(createCustomError(`No task with id: ${taskID} was found`, 404));
         }
 
         res.status(200).json({
-            message: 'Task deleted successfully!'
+            msg: 'Task deleted successfully!'
         });
 
 });
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,5 +1,11 @@
-const errorHandlerMiddleware = (err, req, res, next)=> {
-    console.error(err);
+const {CustomAPIError} = require('../errors/custom-error.js');
+
+const errorHandlerMiddleware = (err, req, res, next)=>{
+    if(err instanceof CustomAPIError){
+        return res.status(err.statusCode).json({
+            msg: err.message
+        });
+    }
     return res.status(500).json({
         msg: 'Something went wrong'
     });
